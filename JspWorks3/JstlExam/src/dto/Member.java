8ed5e80d189c139fd6ac44c4daaa12dcd5abbba8
juhyn88 <68534373+juhyn88@users.noreply.github.com>
2020-11-06 16:36:13 +0900

    Add files via upload

---
+++ JspWorks3/JstlExam/src/dto/Member.java
@@ -0,0 +1,42 @@
+package dto;
+
+
+public class Member {		
+	private String id;
+	private String pwd;
+	private String name;
+	
+	public Member() {	
+			super();
+	}
+
+	public Member(String id, String pwd, String name) {
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+	
